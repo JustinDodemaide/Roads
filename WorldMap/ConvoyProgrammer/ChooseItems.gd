9d3d40b63616b67bd30751df72176a062dcfae28
JustinDodemaide <103222511+JustinDodemaide@users.noreply.github.com>
2023-12-15 01:51:45 -0800

    ConvoyMenu
    Also fixed convoy WorldMapObjects not being updated upon removal of convoy

---
+++ WorldMap/ConvoyProgrammer/ChooseItems.gd
@@ -0,0 +1,12 @@
+extends Node
+
+# ChooseItems
+# Choose what items to pick up/drop off from the selected location
+
+var state_machine = null
+
+func enter(_msg := {}) -> void:
+	pass
+	
+func exit() -> void:
+	pass
---
+++ WorldMap/ConvoyProgrammer/ChooseVehicles.gd
@@ -0,0 +1,13 @@
+extends Node
+
+# ChooseVehicles
+# The player needs to select what vehicles from their current location
+# are to be in the convoy.
+
+var state_machine = null
+
+func enter(_msg := {}) -> void:
+	pass
+	
+func exit() -> void:
+	pass
