90fdd3d8b1ed40dca9916022ff95d757eb5b4f9b
JustinDodemaide <103222511+JustinDodemaide@users.noreply.github.com>
2023-12-28 23:35:49 -0800

    WorldGen - Lakes, mountains, minor adjustments

--- World/WorldGenerator.gd
+++ World/WorldGenerator.gd
@@ -1,9 +1,12 @@
+extends Node
 class_name WorldGenerator
 
 # Zone rings
 # Altitudes
 # Other geography (impasses, swamps, etc)
 # World Objects
+
+# Not very optimized, but only runs once per playthrough
 
 var tilemap
 const TILE_SIZE = 16
@@ -14,11 +17,12 @@ func execute(tilemap_to_be_altered:TileMap):
 	tilemap = tilemap_to_be_altered
 	
 	zone_rings()
-	set_world_objects1()
+	await altitudes()
+	set_world_objects()
 
 func zone_rings():
-	const ZONE_RADIUS = [35,25,12]
-	const LAYER_NAMES = ["grass","desert","mountain"]
+	const ZONE_RADIUS_MULTIPLIER:int = 14
+	const ZONE_RADIUS_RATIOS = [3,2,1]
 	# Start with outermost circle then do innermost rings
 	# x = rcos(θ)
 	# y = rsin(θ)
@@ -28,70 +32,126 @@ func zone_rings():
 	const CENTER = Vector2i(250,250)
 	for layer in NUMBER_OF_ZONES:
 		for theta in 360:
-			for radius in ZONE_RADIUS[layer]:
-				# If the layer isn't grasslands, we can add some noise
-				# at the edges of the regions to make it look more natural
-				# if layer != 0 and 
-				if radius == ZONE_RADIUS[layer] - 1:
-					radius += randi_range(-2,3)
+			for radius in ZONE_RADIUS_RATIOS[layer] * ZONE_RADIUS_MULTIPLIER:
+				# This adds some randomness to the edges of the regions that
+				# make them look more natural
+				if radius == (ZONE_RADIUS_RATIOS[layer] * ZONE_RADIUS_MULTIPLIER) - 1:
+					radius += randi_range(-3,3)
 				var x = round(radius * cos(theta))
 				var y = round(radius * sin(theta))
 				
 				var tile = Vector2i(x,y) + CENTER
-				var atlas = Vector2i(layer,0)
+				var atlas = Vector2i(1,layer)
 				tilemap.set_cell(FLOOR_LAYER,tile,0,atlas)
+				# All cells at this point are placeholders - they'll be
+				# changed in altitudes()
+
+#func noise_tester():
+	## This function allowed me to see the results of each type/frequency of noise
+	## without having to rerun the program each time. Leaving it here in case
+	## I need it again.
+	#var button = Global.world.get_node("Button")
+	#
+	#var noise = FastNoiseLite.new()
+	#var dots = []
+	#for i in 6:
+		#noise.noise_type = i
+		#
+		#var step = 0.001
+		#for j in 3:
+			#button.text = str(i) + "\n" + str(step)
+			#for dot in dots:
+				#dot.queue_free()
+			#dots.clear()
+			#
+			#noise.set_frequency(step)
+			#
+			#for tile in tilemap.get_used_cells(FLOOR_LAYER):
+				#var sprite = Sprite2D.new()
+				#sprite.texture = load("res://dot.png")
+				#sprite.position = tile * Vector2i(TILE_SIZE,TILE_SIZE) + Vector2i(TILE_SIZE/2,TILE_SIZE/2)
+				#var val = abs(noise.get_noise_2dv(tile))
+				#sprite.modulate = Color.BLACK
+				#sprite.modulate.a = val
+				#dots.append(sprite)
+				#Global.world.add_child(sprite)
+				#
+			#await button.pressed
+			#
+			#step *= 10
+			#
+		#await button.pressed
+
+func altitudes():
+	# Potential noises:
+	# Simplex 0.01
+	# Type Value Cubic (4) 0.1 (water threshold 0.05)
+	# Type Value (5) 0.1
+	var noise = FastNoiseLite.new()
+	noise.noise_type = FastNoiseLite.TYPE_SIMPLEX
+	noise.set_frequency(0.05)
+	randomize()
+	noise.seed = randi()
+	
+	var cells = tilemap.get_used_cells(FLOOR_LAYER)
+	
+	# Originally planned on getting altitude-tile threshold values automatically
+	# so I could change the noise type/frequency freely, but it wasn't giving the
+	# results I wanted (too much water, not enough mountains).
+	
+	#var highest_value:float = 0
+	#var lowest_value:float = 1000000
+	#for cell in cells:
+		#var val = abs(noise.get_noise_2dv(cell))
+		#if val > highest_value:
+			#highest_value = val
+		#if val < lowest_value:
+			#lowest_value = val
+	
+	#const NUM_STEPS:int = 5
+	#var step = (highest_value - lowest_value) / NUM_STEPS
+	#for cell in cells:
+		#var temp_step = lowest_value + step
+		#var val = abs(noise.get_noise_2dv(cell))
+		#var atlas_x_value:int = 0
+		#for i in NUM_STEPS:
+			#if val <= temp_step:
+				#break
+			#atlas_x_value += 1
+			#temp_step += temp_step
+		#var zone = tilemap.get_cell_tile_data(FLOOR_LAYER,cell).get_custom_data("zone")
+		#var atlas = Vector2i(atlas_x_value,zone)
+		#tilemap.set_cell(FLOOR_LAYER,cell,0,atlas)
+		
+	# Hard coding the values gave better results. I'll revisit the automatic
+	# version if I need the flexibility.
+	# With Simplex 0.05, the highest value was ~0.8, the lowest was 0.
+	for cell in cells:
+		var val = abs(noise.get_noise_2dv(cell))
+		var atlas_x_value:int
+		if val < 0.075:
+			atlas_x_value = 0
+		if val >= 0.075 and val < 0.235:
+			atlas_x_value = 1
+		if val >= 0.235 and val < 0.48:
+			atlas_x_value = 2
+		if val >= 0.48 and val < 0.64:
+			atlas_x_value = 3
+		if val >= 0.64:
+			atlas_x_value = 4
+		var zone = tilemap.get_cell_tile_data(FLOOR_LAYER,cell).get_custom_data("zone")
+		var atlas = Vector2i(atlas_x_value,zone)
+		tilemap.set_cell(FLOOR_LAYER,cell,0,atlas)
 
 func set_world_objects():
-	# Want exactly x objects per zone
-	# Want the objects to be spread evenly (but still randomly) around the map
-
-	# Place one object on random tile
-	# Draw a circle with a radius of 'desired_distance' around the object
-	# Remove every tile of circle from 'available tiles' pool
-	const MIN_DISTANCE:int = 5
-	const NEEDED_OBJECTS:int = 15
-	var objects_placed:int = 0
-	const MAX_OBJECTS_PER_ZONE:int = 5
-	var objects_in_zone = [0,0,0]
-	var available_positions = tilemap.get_used_cells(FLOOR_LAYER)
-	while objects_placed < NEEDED_OBJECTS:
-		if available_positions.is_empty():
-			break
-		# Get a random position from the list of available positions
-		var pos = available_positions[randi_range(0,available_positions.size() - 1)]
-		# Get the zone that the position is in
-		var zone = tilemap.get_cell_tile_data(FLOOR_LAYER,pos).get_custom_data("zone")
-		if objects_in_zone[zone] >= MAX_OBJECTS_PER_ZONE:
-			# If the zone already has all its required objects, 
-			continue
-		for theta in 360:
-			for radius in MIN_DISTANCE:
-				# If the layer isn't grasslands, we can add some noise
-				# at the edges of the regions to make it look more natural
-				# if layer != 0 and 
-				var x = round(radius * cos(theta))
-				var y = round(radius * sin(theta))
-				var pos_to_remove = Vector2i(x,y) + pos
-				available_positions.erase(pos_to_remove)
-		var sprite = Sprite2D.new()
-		sprite.texture = load("res://dot.png")
-		sprite.position = pos * Vector2i(TILE_SIZE,TILE_SIZE)
-		Global.world.add_child(sprite)
-		objects_placed += 1
-	if NEEDED_OBJECTS - objects_placed == 0:
-		print("All objects placed")
-	else:
-		print(NEEDED_OBJECTS - objects_placed, " objects haven't been placed")
-
-func set_world_objects1():
-	const OBJECTS_PER_ZONE:int = 100
+	const OBJECTS_PER_ZONE_MULTIPLIER:int = 100
+	const OBJECTS_PER_ZONE_RATIOS:Array[float] = [1,0.75,0.33]
 	var zone_tiles = get_tiles_by_zone()
 	for zone in NUMBER_OF_ZONES:
 		var available_positions = zone_tiles[zone]
-		pass
-		for i in OBJECTS_PER_ZONE:
+		for i in OBJECTS_PER_ZONE_RATIOS[zone] * OBJECTS_PER_ZONE_MULTIPLIER:
 			if available_positions.is_empty():
-				print(OBJECTS_PER_ZONE - i, " unplaced objects in zone ", zone)
+				print((OBJECTS_PER_ZONE_RATIOS[zone] * OBJECTS_PER_ZONE_MULTIPLIER) - i, " unplaced objects in zone ", zone)
 				break
 			var pos = available_positions[randi_range(0,available_positions.size() - 1)]
 			pass
@@ -103,7 +163,10 @@ func get_tiles_by_zone():
 	for i in NUMBER_OF_ZONES:
 		zones.append([])
 	for tile in tilemap.get_used_cells(FLOOR_LAYER):
-		var zone = tilemap.get_cell_tile_data(FLOOR_LAYER,tile).get_custom_data("zone")
+		var cell_data = tilemap.get_cell_tile_data(FLOOR_LAYER,tile)
+		if cell_data.get_custom_data("impass") == true:
+			continue
+		var zone = cell_data.get_custom_data("zone")
 		zones[zone].append(tile)
 	return zones
 
