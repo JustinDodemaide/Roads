49b875167d27370e96bc579765ee2edab471c6ed
JustinDodemaide <103222511+JustinDodemaide@users.noreply.github.com>
2024-02-28 23:30:45 -0800

    Fixed Foundry menu

--- Level/LevelScenes/FoundryMenu/FoundryMenu.gd
+++ Level/LevelScenes/FoundryMenu/FoundryMenu.gd
@@ -1,19 +1,21 @@
 extends Node
 
 @export var column:VBoxContainer
-@export var column_item:Control
 @export var selection_panel:PanelContainer
 var selected_item:Item
+
+var item_container:PackedScene = preload("res://Level/LevelScenes/FoundryMenu/ItemColumnItem/FoundryItem.tscn")
 
 func _ready():
 	# Based on research, make a list of items that are available
 	var all_items:PackedStringArray = ["Waffle"]
 	all_items.sort()
 	for string in all_items:
-		var new_column_item = column_item.duplicate()
 		var path:String = "res://Items/" + string + "/" + string + ".tres"
 		var item:Resource = load(path)
+		var new_column_item = item_container.instantiate()
 		new_column_item.init(item)
+		new_column_item.item_selected.connect(_on_item_selected)
 		column.add_child(new_column_item)
 
 func _on_item_selected(item):
